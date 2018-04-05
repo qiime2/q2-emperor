f1886281a10faf8d27b95ade1720a96fd9584b6d
Evan Bolyen <ebolyen@gmail.com>
2018-04-05 14:41:27 -0400

    DOC: add citations

--- q2_emperor/plugin_setup.py
+++ q2_emperor/plugin_setup.py
@@ -10,23 +10,15 @@
 import q2_emperor
 from ._plot import plot
 
-from qiime2.plugin import Plugin, Metadata, Str, List
+from qiime2.plugin import Plugin, Metadata, Str, List, Citations
 from q2_types.ordination import PCoAResults
-
 
 plugin = Plugin(
     name='emperor',
     version=q2_emperor.__version__,
     website='http://emperor.microbio.me',
     package='q2_emperor',
-    citation_text=('EMPeror: a tool for visualizing high-'
-                   'throughput microbial community data.\nVazquez-Baeza Y, '
-                   'Pirrung M, Gonzalez A, Knight R.\nGigascience. '
-                   '2013 Nov 26;2(1):16.\n'
-                   'Bringing the Dynamic Microbiome to Life with Animations.\n'
-                   'Vazquez-Baeza Y, Gonzalez A, Smarr L, McDonald D, Morton '
-                   'J, Navas-Molina J, Knight R.\nCell Host & Microbe.'
-                   'Volume 21, Issue 1, p7–10, 11 January 2017.'),
+    citations=Citations.load('CITATIONS.bib', package='q2_emperor'),
     description=('This QIIME 2 plugin wraps Emperor and '
                  'supports interactive visualization of ordination '
                  'plots.'),
