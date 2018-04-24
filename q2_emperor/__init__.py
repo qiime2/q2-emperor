7b7e47d0ab1cc0db6b920cb67883cee2454e007a
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2018-04-24 10:20:04 -0700

    ENH: Adds procrustes plots to q2-emperor (#60)

--- q2_emperor/__init__.py
+++ q2_emperor/__init__.py
@@ -6,10 +6,10 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from ._plot import plot
+from ._plot import plot, procrustes_plot
 from ._version import get_versions
 
 __version__ = get_versions()['version']
 del get_versions
 
-__all__ = ['plot']
+__all__ = ['plot', 'procrustes_plot']
--- q2_emperor/_plot.py
+++ q2_emperor/_plot.py
@@ -17,16 +17,44 @@ from emperor import Emperor
 TEMPLATES = pkg_resources.resource_filename('q2_emperor', 'assets')
 
 
-def plot(output_dir: str, pcoa: skbio.OrdinationResults,
-         metadata: qiime2.Metadata, custom_axes: str=None) -> None:
+def _generic_plot(output_dir: str, master: skbio.OrdinationResults,
+                  metadata: qiime2.Metadata,
+                  other_pcoa: skbio.OrdinationResults, plot_name,
+                  custom_axes: str=None):
 
     mf = metadata.to_dataframe()
-    viz = Emperor(pcoa, mf, remote='.')
 
-    html = viz.make_emperor(standalone=True, custom_axes=custom_axes)
+    if other_pcoa is None:
+        procrustes = None
+    else:
+        procrustes = [other_pcoa]
+
+    viz = Emperor(master, mf, procrustes=procrustes, remote='.')
+
+    if custom_axes is not None:
+        viz.custom_axes = custom_axes
+
+    if other_pcoa:
+        viz.procrustes_names = ['reference', 'other']
+
+    html = viz.make_emperor(standalone=True)
     viz.copy_support_files(output_dir)
     with open(os.path.join(output_dir, 'emperor.html'), 'w') as fh:
         fh.write(html)
 
     index = os.path.join(TEMPLATES, 'index.html')
-    q2templates.render(index, output_dir)
+    q2templates.render(index, output_dir, context={'plot_name': plot_name})
+
+
+def plot(output_dir: str, pcoa: skbio.OrdinationResults,
+         metadata: qiime2.Metadata, custom_axes: str=None) -> None:
+    _generic_plot(output_dir, master=pcoa, metadata=metadata, other_pcoa=None,
+                  custom_axes=custom_axes, plot_name='plot')
+
+
+def procrustes_plot(output_dir: str, reference_pcoa: skbio.OrdinationResults,
+                    other_pcoa: skbio.OrdinationResults,
+                    metadata: qiime2.Metadata, custom_axes: str=None) -> None:
+    _generic_plot(output_dir, master=reference_pcoa, metadata=metadata,
+                  other_pcoa=other_pcoa, custom_axes=custom_axes,
+                  plot_name='procrustes_plot')
